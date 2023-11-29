c31aefa49cd9e62738743e9eec45d04ed7139b8f
UVSRajyasree <uppalurirajyasree@gmail.com>
2023-11-29 15:11:26 +0530

    intial commit

---
+++ BASICFIRSTJAVAPROGRAM/src/javaprogram/CheckedException.java
@@ -0,0 +1,15 @@
+package javaprogram;
+
+public class CheckedException {
+
+	public static void main(String[] args) throws Exception {
+		// TODO Auto-generated method stub
+		System.out.println("Hi");
+
+		//try {
+		Thread.sleep(3000);
+		//}catch(Exception e) {}
+	System.out.println("Hello..");
+	}
+
+}
--- BASICFIRSTJAVAPROGRAM/src/javaprogram/ThrowsException.java
+++ BASICFIRSTJAVAPROGRAM/src/javaprogram/ThrowsException.java
@@ -1,10 +1,26 @@
 package javaprogram;
 
 public class ThrowsException {
+	static void dis1() throws Exception{
+		//try {
+			int res = 100/0;
+		//}catch(Exception e) {}
+		System.out.println("This is dis1 method");
+	}
+	static void dis2() throws Exception{
+		//try {
+		dis1();
+		//}catch(Exception e) {}
+		System.out.println("This is dis2 method");
+	}
 
-	public static void main(String[] args) {
+	public static void main(String[] args) throws Exception {
 		// TODO Auto-generated method stub
-
+		try {
+			dis2();
+			}catch(Exception e) {}
+			System.out.println("This is main method");
+		
 	}
 
 }
