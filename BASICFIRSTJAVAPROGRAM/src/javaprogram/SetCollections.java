fe4d0b0cdb668d4d00abbe4acc7cf102b9882d6e
UVSRajyasree <uppalurirajyasree@gmail.com>
2023-12-01 18:02:51 +0530

    intial commit

---
+++ BASICFIRSTJAVAPROGRAM/src/javaprogram/SetCollections.java
@@ -0,0 +1,40 @@
+package javaprogram;
+
+import java.util.HashSet;
+import java.util.LinkedHashSet;
+import java.util.Set;
+import java.util.TreeSet;
+public class SetCollections {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		// HashSet UnOrdered
+			//	Set ss1 = new HashSet();
+			//LinkedHashSet Ordered
+				//Set ss1 = new LinkedHashSet();
+			// TreeSEt Ordered Sorted In Ascending Order Only Same Data Type
+				Set ss1 = new TreeSet();
+				System.out.println("Size "+ss1.size());
+				ss1.add(10);
+				ss1.add(30);
+				ss1.add(20);
+				ss1.add(10);
+				ss1.add(10.10);
+				ss1.add(60);
+				ss1.add(50);
+				System.out.println("Size "+ss1.size());
+				System.out.println(ss1);
+		//	    System.out.println("Remove "+ss1.remove(30));
+	  //        System.out.println("Remove "+ss1.remove(300));
+		//		System.out.println("Size "+ss1.size());
+		//		System.out.println(ss1);
+		//		System.out.println("Search "+ss1.contains(30));
+				System.out.println("Search "+ss1.contains(10));
+		//		ss1.clear();
+		//		System.out.println("Size "+ss1.size());
+		//		System.out.println(ss1);
+		//		System.out.println("Empty "+ss1.isEmpty());
+	}
+
+}
