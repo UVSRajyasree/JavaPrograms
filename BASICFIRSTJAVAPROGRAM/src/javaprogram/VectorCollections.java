d3ab9c30c4d3f05a393a4421e0fd4744b7332956
UVSRajyasree <uppalurirajyasree@gmail.com>
2023-12-04 14:18:10 +0530

    intial commit

---
+++ BASICFIRSTJAVAPROGRAM/src/javaprogram/VectorCollections.java
@@ -0,0 +1,17 @@
+package javaprogram;
+
+import java.util.Vector;
+
+public class VectorCollections {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		Vector vv = new Vector();
+		vv.add(10);
+		vv.add(20);
+		vv.add(30);
+		System.out.println(vv);
+	}
+
+}
